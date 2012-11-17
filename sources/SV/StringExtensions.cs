3ec59f313ffa88d09ac6cac498fe13861c8080fd
Sergii Vashchyshchuk <SWashchyshchuk@gmail.com>
2012-11-17 22:32:40 +1100

    Added SV project which contains common logic

---
+++ sources/SV/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SV
+{
+    using System;
+
+    public static class StringExtensions
+    {
+        public static string F(this string st, params object[] args)
+        {
+            return String.Format(st, args);
+        }
+
+        public static string[] SplitIntoLines(this string text)
+        {
+            return text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string[] SplitIntoLines(this string text, int maxNumberOfLines)
+        {
+            return text.Split(new char[] { '\r', '\n' }, maxNumberOfLines, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ sources/SV/XDocumentExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SV
+{
+    using System.Text;
+    using System.Xml.Linq;
+
+    public static class XDocumentExtensions
+    {
+        public static string ToStringWithDeclaration(this XDocument document)
+        {
+            var result = new StringBuilder();
+
+            if (document.Declaration != null)
+            {
+                result.AppendLine(document.Declaration.ToString());
+            }
+
+            result.AppendLine(document.ToString());
+
+            return result.ToString();
+        }
+    }
+}

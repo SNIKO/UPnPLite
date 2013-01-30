b3f2efcfc44da6606b3c16f1c49dbb88983378f7
Sergii.Vashchyshchuk <SWashchyshchuk@gmail.com>
2013-01-30 22:52:59 +1100

    - Added project for Windows Phone 8

---
+++ sources/Phone/SV.UPnPLite/Extensions/WebRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SV.UPnPLite.Extensions
+{
+    using System.IO;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     Defines extension methods for <see cref="WebRequest"/>.
+    /// </summary>
+    public static class WebRequestExtensions
+    {
+        public static Task<Stream> GetRequestStreamAsync(this WebRequest instance)
+        {
+            var task = Task.Factory.FromAsync(instance.BeginGetRequestStream, asyncResult => instance.EndGetRequestStream(asyncResult), null);
+
+            return task;
+        }
+
+        public static Task<WebResponse> GetResponseAsync(this WebRequest instance)
+        {
+            var task = Task.Factory.FromAsync(instance.BeginGetResponse, asyncResult => instance.EndGetResponse(asyncResult), null);
+
+            return task;
+        }
+    }
+}
